5e683486179b8df5e930d75ac97c2e49ab03bb75
Amirhossein <amirsorouri26@gmail.com>
2019-04-30 19:50:14 +0430

    and finally a good looking search engine gui with acceptable backend implemented by django. i have to bring an awesome crawler and i am done.

---
+++ projects/Search Engine Project in Python/Search Engine Python/Search Engine/python-search-engine-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/amirsorouri00/Desktop/search-engine/projects/Search Engine Project in Python/Search Engine Python/Search Engine/python-search-engine-env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
